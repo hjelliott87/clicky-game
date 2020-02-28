c2c136ca783b89b09e1c1203d946208ae77346b9
Henry Elliott <hjelliott1987@gmail.com>
2020-02-27 21:49:21 -0600

    Updates

---
+++ precache-manifest.effaec55d05c1498bd8a5c8c8a92c73a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3540f7cebad6d92cbbb599a2decdb062",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "90afdfb47bbad9762a8f",
+    "url": "/clicky-game/static/css/main.32d4f831.chunk.css"
+  },
+  {
+    "revision": "7fd9db456720a0375670",
+    "url": "/clicky-game/static/js/2.86ffefb2.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/clicky-game/static/js/2.86ffefb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90afdfb47bbad9762a8f",
+    "url": "/clicky-game/static/js/main.94fc7b37.chunk.js"
+  },
+  {
+    "revision": "c009a0a1e3686787466f",
+    "url": "/clicky-game/static/js/runtime-main.4231084e.js"
+  }
+]);
